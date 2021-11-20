8ae773bc2833b91c082e3711589af8977880babc
Lucas Duffeck <lucasduffeck@hotmail.com>
2021-11-20 08:17:54 -0500

    Initial Commit

---
+++ TabletopCurrencyTracker/TabletopCurrencyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabletopCurrencyTrackerApp.swift
+//  TabletopCurrencyTracker
+//
+//  Created by user204084 on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TabletopCurrencyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
